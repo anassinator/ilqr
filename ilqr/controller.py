Fix hessians in forward rollout

ilqr/controller.py
@@ -122,14 +122,15 @@ class iLQR(BaseController):
 
             # Forward rollout only if it needs to be recomputed.
             if changed:
-                rollout = self._forward_rollout(x0, us)
-                xs, F_x, F_u, L, L_x, L_u, L_xx, L_ux, L_uu = rollout
+                (xs, F_x, F_u, L, L_x, L_u, L_xx, L_ux, L_uu, F_xx, F_ux,
+                 F_uu) = self._forward_rollout(x0, us)
                 J_opt = L.sum()
                 changed = False
 
             try:
                 # Backward pass.
-                k, K = self._backward_pass(F_x, F_u, L_x, L_u, L_xx, L_ux, L_uu)
+                k, K = self._backward_pass(F_x, F_u, L_x, L_u, L_xx, L_ux, L_uu,
+                                           F_xx, F_ux, F_uu)
 
                 # Backtracking line search.
                 for alpha in alphas:
@@ -249,6 +250,12 @@ class iLQR(BaseController):
                     [N, action_size, state_size].
                 L_uu: Hessian of cost path w.r.t. u, u
                     [N, action_size, action_size].
+                F_xx: Hessian of state path w.r.t. x, x if Hessians are used
+                    [N, state_size, state_size, state_size].
+                F_ux: Hessian of state path w.r.t. u, x if Hessians are used
+                    [N, state_size, action_size, state_size].
+                F_uu: Hessian of state path w.r.t. u, u if Hessians are used
+                    [N, state_size, action_size, action_size].
         """
         state_size = self.dynamics.state_size
         action_size = self.dynamics.action_size
@@ -257,6 +264,15 @@ class iLQR(BaseController):
         xs = np.empty((N + 1, state_size))
         F_x = np.empty((N, state_size, state_size))
         F_u = np.empty((N, state_size, action_size))
+
+        if self._use_hessians:
+            F_xx = np.empty((N, state_size, state_size, state_size))
+            F_ux = np.empty((N, state_size, action_size, state_size))
+            F_uu = np.empty((N, state_size, action_size, action_size))
+        else:
+            F_xx = None
+            F_ux = None
+            F_uu = None
 
         L = np.empty(N + 1)
         L_x = np.empty((N + 1, state_size))
@@ -281,19 +297,47 @@ class iLQR(BaseController):
             L_ux[i] = self.cost.l_ux(x, u, i, terminal=False)
             L_uu[i] = self.cost.l_uu(x, u, i, terminal=False)
 
+            if self._use_hessians:
+                F_xx[i] = self.dynamics.f_xx(x, u, i)
+                F_ux[i] = self.dynamics.f_ux(x, u, i)
+                F_uu[i] = self.dynamics.f_uu(x, u, i)
+
         x = xs[-1]
         L[-1] = self.cost.l(x, None, N, terminal=True)
         L_x[-1] = self.cost.l_x(x, None, N, terminal=True)
         L_xx[-1] = self.cost.l_xx(x, None, N, terminal=True)
 
-        return xs, F_x, F_u, L, L_x, L_u, L_xx, L_ux, L_uu
+        return xs, F_x, F_u, L, L_x, L_u, L_xx, L_ux, L_uu, F_xx, F_ux, F_uu
 
-    def _backward_pass(self, F_x, F_u, L_x, L_u, L_xx, L_ux, L_uu):
+    def _backward_pass(self,
+                       F_x,
+                       F_u,
+                       L_x,
+                       L_u,
+                       L_xx,
+                       L_ux,
+                       L_uu,
+                       F_xx=None,
+                       F_ux=None,
+                       F_uu=None):
         """Computes the feedforward and feedback gains k and K.
 
         Args:
-            xs: State path [N+1, state_size].
-            us: Control path [N, action_size].
+            F_x: Jacobian of state path w.r.t. x [N, state_size, state_size].
+            F_u: Jacobian of state path w.r.t. u [N, state_size, action_size].
+            L_x: Jacobian of cost path w.r.t. x [N+1, state_size].
+            L_u: Jacobian of cost path w.r.t. u [N, action_size].
+            L_xx: Hessian of cost path w.r.t. x, x
+                [N+1, state_size, state_size].
+            L_ux: Hessian of cost path w.r.t. u, x [N, action_size, state_size].
+            L_uu: Hessian of cost path w.r.t. u, u
+                [N, action_size, action_size].
+            F_xx: Hessian of state path w.r.t. x, x if Hessians are used
+                [N, state_size, state_size, state_size].
+            F_ux: Hessian of state path w.r.t. u, x if Hessians are used
+                [N, state_size, action_size, state_size].
+            F_uu: Hessian of state path w.r.t. u, u if Hessians are used
+                [N, state_size, action_size, action_size].
 
         Returns:
             Tuple of
@@ -307,9 +351,14 @@ class iLQR(BaseController):
         K = np.empty_like(self._K)
 
         for i in range(self.N - 1, -1, -1):
-            Q_x, Q_u, Q_xx, Q_ux, Q_uu = self._Q(F_x[i], F_u[i], L_x[i], L_u[i],
-                                                 L_xx[i], L_ux[i], L_uu[i], V_x,
-                                                 V_xx)
+            if self._use_hessians:
+                Q_x, Q_u, Q_xx, Q_ux, Q_uu = self._Q(
+                    F_x[i], F_u[i], L_x[i], L_u[i], L_xx[i], L_ux[i], L_uu[i],
+                    V_x, V_xx, F_xx[i], F_ux[i], F_uu[i])
+            else:
+                Q_x, Q_u, Q_xx, Q_ux, Q_uu = self._Q(F_x[i], F_u[i], L_x[i],
+                                                     L_u[i], L_xx[i], L_ux[i],
+                                                     L_uu[i], V_x, V_xx)
 
             # Eq (6).
             k[i] = -np.linalg.solve(Q_uu, Q_u)
@@ -326,16 +375,39 @@ class iLQR(BaseController):
 
         return np.array(k), np.array(K)
 
-    def _Q(self, f_x, f_u, l_x, l_u, l_xx, l_ux, l_uu, V_x, V_xx):
+    def _Q(self,
+           f_x,
+           f_u,
+           l_x,
+           l_u,
+           l_xx,
+           l_ux,
+           l_uu,
+           V_x,
+           V_xx,
+           f_xx=None,
+           f_ux=None,
+           f_uu=None):
         """Computes second order expansion.
 
         Args:
-            x: State [state_size].
-            u: Control [action_size].
-            V_x: d/dx of the value function at the next time step [state_size].
-            V_xx: d^2/dx^2 of the value function at the next time step
-                [state_size, state_size].
-            i: Current time step.
+            F_x: Jacobian of state w.r.t. x [state_size, state_size].
+            F_u: Jacobian of state w.r.t. u [state_size, action_size].
+            L_x: Jacobian of cost w.r.t. x [state_size].
+            L_u: Jacobian of cost w.r.t. u [action_size].
+            L_xx: Hessian of cost w.r.t. x, x [state_size, state_size].
+            L_ux: Hessian of cost w.r.t. u, x [action_size, state_size].
+            L_uu: Hessian of cost w.r.t. u, u [action_size, action_size].
+            V_x: Jacobian of the value function at the next time step
+                [state_size].
+            V_xx: Hessian of the value function at the next time step w.r.t.
+                x, x [state_size, state_size].
+            F_xx: Hessian of state w.r.t. x, x if Hessians are used
+                [state_size, state_size, state_size].
+            F_ux: Hessian of state w.r.t. u, x if Hessians are used
+                [state_size, action_size, state_size].
+            F_uu: Hessian of state w.r.t. u, u if Hessians are used
+                [state_size, action_size, action_size].
 
         Returns:
             Tuple of
@@ -356,10 +428,6 @@ class iLQR(BaseController):
         Q_uu = l_uu + f_u.T.dot(V_xx + reg).dot(f_u)
 
         if self._use_hessians:
-            f_xx = self.dynamics.f_xx(x, u, i)
-            f_ux = self.dynamics.f_ux(x, u, i)
-            f_uu = self.dynamics.f_uu(x, u, i)
-
             Q_xx += np.tensordot(V_x, f_xx, axes=1)
             Q_ux += np.tensordot(V_x, f_ux, axes=1)
             Q_uu += np.tensordot(V_x, f_uu, axes=1)