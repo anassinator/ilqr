871d39a010007c814ed36c205ba69c827d4bf8d1
nathaniel <nagoldfarb@wpi.edu>
2020-09-20 20:24:33 -0400

    path following working with MPC

--- ilqr/controller.py
+++ ilqr/controller.py
@@ -516,3 +516,71 @@ class RecedingHorizonController(object):
             us_end = self._random.uniform(-1, 1, (step_size, action_size))
             us_init = np.vstack([us_start, us_end])
             n_iterations = subsequent_n_iterations
+
+
+class RecedingHorizonControllerPath(RecedingHorizonController):
+
+    def __init__(self,  x0, controller):
+
+        super(RecedingHorizonControllerPath, self).__init__(x0, controller)
+
+
+    def control(self,
+                us_init,
+                step_size=1,
+                initial_n_iterations=100,
+                subsequent_n_iterations=1,
+                *args,
+                **kwargs):
+        """Yields the optimal controls to run at every step as a receding
+        horizon problem.
+
+        Note: The first iteration will be slow, but the successive ones will be
+        significantly faster.
+
+        Note: This will automatically move the current controller's state to
+        what the dynamics model believes will be the next state after applying
+        the entire control path computed. Should you want to correct this state
+        between iterations, simply use the `set_state()` method.
+
+        Note: If your cost or dynamics are time dependent, then you might need
+        to shift their internal state accordingly.
+
+        Args:
+            us_init: Initial control path [N, action_size].
+            step_size: Number of steps between each controller fit. Default: 1.
+                i.e. re-fit at every time step. You might need to increase this
+                depending on how powerful your machine is in order to run this
+                in real-time.
+            initial_n_iterations: Initial max number of iterations to fit.
+                Default: 100.
+            subsequent_n_iterations: Subsequent max number of iterations to
+                fit. Default: 1.
+            *args, **kwargs: Additional positional and key-word arguments to
+                pass to `controller.fit()`.
+
+        Yields:
+            Tuple of
+                xs: optimal state path [step_size+1, state_size].
+                us: optimal control path [step_size, action_size].
+        """
+        action_size = self._controller.dynamics.action_size
+        n_iterations = initial_n_iterations
+        while True:
+            xs, us = self._controller.fit(self._x,
+                                          us_init,
+                                          n_iterations=n_iterations,
+                                          *args,
+                                          **kwargs)
+            self._x = xs[step_size]
+            self._controller.cost.update_step(step_size)
+            yield xs[:step_size + 1], us[:step_size]
+
+            # Set up next action path seed by simply moving along the current
+            # optimal path and appending random unoptimal values at the end.
+            us_start = us[step_size:]
+            us_end = self._random.uniform(-1, 1, (step_size, action_size))
+            us_init = np.vstack([us_start, us_end])
+            n_iterations = subsequent_n_iterations
+
+
--- ilqr/cost.py
+++ ilqr/cost.py
@@ -413,6 +413,8 @@ class BatchAutoDiffCost(Cost):
 
         # Terminal cost only depends on x, so we only need to evaluate the x
         # partial derivatives.
+
+
         l_tensor_term = f(x_rep_1, None, i, terminal=True)[0]
         J_x_term, _ = T.grad(l_tensor_term,
                              inputs_term,
@@ -804,7 +806,6 @@ class QRCost(Cost):
         R = self.R
         x_diff = x - self.x_goal
         squared_x_cost = x_diff.T.dot(Q).dot(x_diff)
-
         if terminal:
             return squared_x_cost
 
@@ -937,6 +938,7 @@ class PathQRCost(Cost):
         self._R_plus_R_T = self.R + self.R.T
         self._Q_plus_Q_T_terminal = self.Q_terminal + self.Q_terminal.T
 
+        super(PathQRCost, self).__init__()
         super(PathQRCost, self).__init__()
 
     def l(self, x, u, i, terminal=False):
@@ -1176,6 +1178,164 @@ class PathQsRCost(Cost):
             d^2l/dudx [action_size, state_size].
         """
         return np.zeros((self.R.shape[0], self.Q[i].shape[0]))
+
+    def l_uu(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to u.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/du^2 [action_size, action_size].
+        """
+        if terminal:
+            return np.zeros_like(self.R)
+
+        return self._R_plus_R_T
+
+class PathQRCostMPC(Cost):
+
+    """Quadratic Regulator Instantaneous Cost for trajectory following."""
+
+    def __init__(self, Q, R, x_path, u_path=None, Q_terminal=None):
+        """Constructs a QRCost.
+
+        Args:
+            Q: Quadratic state cost matrix [state_size, state_size].
+            R: Quadratic control cost matrix [action_size, action_size].
+            x_path: Goal state path [N+1, state_size].
+            u_path: Goal control path [N, action_size].
+            Q_terminal: Terminal quadratic state cost matrix
+                [state_size, state_size].
+        """
+        self.Q = np.array(Q)
+        self.R = np.array(R)
+        self.x_path = np.array(x_path)
+
+        state_size = self.Q.shape[0]
+        action_size = self.R.shape[0]
+        path_length = self.x_path.shape[0]
+
+        if Q_terminal is None:
+            self.Q_terminal = self.Q
+        else:
+            self.Q_terminal = np.array(Q_terminal)
+
+        if u_path is None:
+            self.u_path = np.zeros(path_length - 1, action_size)
+        else:
+            self.u_path = np.array(u_path)
+
+        assert self.Q.shape == self.Q_terminal.shape, "Q & Q_terminal mismatch"
+        assert self.Q.shape[0] == self.Q.shape[1], "Q must be square"
+        assert self.R.shape[0] == self.R.shape[1], "R must be square"
+        assert state_size == self.x_path.shape[1], "Q & x_path mismatch"
+        assert action_size == self.u_path.shape[1], "R & u_path mismatch"
+        assert path_length == self.u_path.shape[0] + 1, \
+                "x_path must be 1 longer than u_path"
+
+        # Precompute some common constants.
+        self._Q_plus_Q_T = self.Q + self.Q.T
+        self._R_plus_R_T = self.R + self.R.T
+        self._Q_plus_Q_T_terminal = self.Q_terminal + self.Q_terminal.T
+
+        super(PathQRCostMPC, self).__init__()
+
+    def update_step(self, step_size):
+
+        self.x_path = self.x_path[step_size:]
+        self.u_path = self.u_path[step_size:]
+        self.x_path = np.append(self.x_path, [self.x_path[-1].tolist()], axis=0)
+        self.u_path = np.append(self.u_path, [self.u_path[-1].tolist()], axis=0)
+
+
+    def l(self, x, u, i, terminal=False):
+        """Instantaneous cost function.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            Instantaneous cost (scalar).
+        """
+        Q = self.Q_terminal if terminal else self.Q
+        R = self.R
+        x_diff = x - self.x_path[i]
+        squared_x_cost = x_diff.T.dot(Q).dot(x_diff)
+
+        if terminal:
+            return squared_x_cost
+
+        u_diff = u - self.u_path[i]
+        return squared_x_cost + u_diff.T.dot(R).dot(u_diff)
+
+    def l_x(self, x, u, i, terminal=False):
+        """Partial derivative of cost function with respect to x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            dl/dx [state_size].
+        """
+        Q_plus_Q_T = self._Q_plus_Q_T_terminal if terminal else self._Q_plus_Q_T
+        x_diff = x - self.x_path[i]
+        return x_diff.T.dot(Q_plus_Q_T)
+
+    def l_u(self, x, u, i, terminal=False):
+        """Partial derivative of cost function with respect to u.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            dl/du [action_size].
+        """
+        if terminal:
+            return np.zeros_like(self.u_path)
+
+        u_diff = u - self.u_path[i]
+        return u_diff.T.dot(self._R_plus_R_T)
+
+    def l_xx(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/dx^2 [state_size, state_size].
+        """
+        return self._Q_plus_Q_T_terminal if terminal else self._Q_plus_Q_T
+
+    def l_ux(self, x, u, i, terminal=False):
+        """Second partial derivative of cost function with respect to u and x.
+
+        Args:
+            x: Current state [state_size].
+            u: Current control [action_size]. None if terminal.
+            i: Current time step.
+            terminal: Compute terminal cost. Default: False.
+
+        Returns:
+            d^2l/dudx [action_size, state_size].
+        """
+        return np.zeros((self.R.shape[0], self.Q.shape[0]))
 
     def l_uu(self, x, u, i, terminal=False):
         """Second partial derivative of cost function with respect to u.
